6ae8d93d84d8ac9f57b0882ac1ac32665c9f80cc
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2025-05-30 20:00:42 +0900

    fixup! feat: setup Storybook for Next.js development

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,31 +1,26 @@
-import type { StorybookConfig } from '@storybook/nextjs';
-import { createRequire } from 'node:module';
-import { join, dirname } from "path"
+import type { StorybookConfig } from "@storybook/nextjs";
+import { createRequire } from "node:module";
+import { join, dirname } from "path";
 
 const require = createRequire(import.meta.url);
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-docs"),
   ],
-  "addons": [
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@storybook/addon-docs')
-  ],
-  "framework": {
-    "name": getAbsolutePath('@storybook/nextjs'),
-    "options": {}
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  staticDirs: ["../public"],
 };
 export default config;
